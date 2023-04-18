d1f5e634151966243dc9eea74d2a01b73f60d44f
Boubacar <bouba0178@gmail.com>
2023-04-18 01:12:04 +0000

    project base created

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="text-3xl font-extrabold mx-auto text-center mt-5">This is it</h1>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
