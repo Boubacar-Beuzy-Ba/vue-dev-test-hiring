a2de6b4b71c29285187016128f6099eebb1d4d9d
Boubacar <bouba0178@gmail.com>
2023-04-19 13:47:21 +0000

    Search Bar and Search function added

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex bg-rose-100 max-w-12 min-h-12 my-4 p-4">
+        <input type="text" placeholder="Search for a movie" class="w-full h-12 p-4 outline-none text-lg text-black">
+    </div>
+</template>
+
+<script>
+
+export default {
+    setup () {
+        return {}
+    }
+}
+</script>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="flex bg-rose-100 max-w-12 min-h-12 my-4 p-4">
+        <input v-model="search" @keyup.enter="searchMovies" type="text" placeholder="Search for a movie" class="w-full h-12 p-4 outline-none text-lg text-black">
+    </div>
     <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 space-x-2 space-y-2 items-center">
         <div v-for="(item, index) in movies" :key="index" class="max-w-96 min-h-full flex flex-col item-center content-center">
             <img :src="item.Poster" :alt="item.Title" class="">
@@ -15,15 +18,35 @@
     export default {
         data() {
             return {
-                movies: []
+                movies: [],
+                search: '',
+                apiKey: '18951a20'
+            }
+        },
+        methods: {
+            searchMovies() {
+                fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${this.search}`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.Response === "True") {
+                        this.movies = data.Search;
+                        console.log(this.movies)
+                    } else {
+                        // Handle error response
+                        console.error("Error fetching movies:", data.Error);
+                    }
+                })
+                .catch(error => {
+                    // Handle fetch error
+                    console.error("Error fetching movies:", error);
+                });
             }
         },
 
         mounted() {
-            const apiKey = "18951a20"
             const searchQuery = "Marvel"
 
-            fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`)
+            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${searchQuery}`)
             .then(res => res.json())
             .then(data => {
                 if (data.Response === "True") {
