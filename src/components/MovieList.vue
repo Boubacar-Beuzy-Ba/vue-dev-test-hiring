c0e2fe2e6bcbc5e3fe1da3e4883e56c577157ca5
Boubacar <bouba0178@gmail.com>
2023-04-21 16:57:06 +0000

    Basic pagination added

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="flex bg-rose-100 max-w-12 min-h-12 my-4 p-4">
-        <input v-model="search" @keyup.enter="searchMovies" type="text" placeholder="Search for a movie" class="w-full h-12 p-4 outline-none md:text-lg text-base text-black">
+    <div class="flex flex-col md:flex-row justify-evenly gap-2 bg-rose-100 max-w-12 md:w-1/2 items-center content-center min-h-12 mx-auto my-4 p-4">
+        <input v-model="search" @keyup.enter="searchMovies" type="text" placeholder="Search a movie by Title" class="form-input border-none w-full h-12 p-4 outline-none md:text-lg text-base text-black">
+        <input v-model="releasedYear" @keyup.enter="searchMoviesbyYear" type="text" placeholder="Search a movie by Year" class="border-none form-input w-full h-12 p-4 outline-none md:text-lg text-base text-black">
+        <select @change="searchMoviesByGenre" name="genre" id="genre" class="w-full h-12 form-select rounded-lg border-none">
+            <option value=""></option>
+            <option :value="genre">Action</option>
+        </select>
     </div>
     <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 space-x-2 space-y-2 items-center">
         <div v-for="(item, index) in movies" :key="index" class="max-w-96 min-h-full flex flex-col item-center content-center">
@@ -8,9 +13,25 @@
             <div class="text-center">
                 <h1>{{ item.Title }}</h1>
                 <p>{{ item.Year }}</p>
-                <p></p>
             </div>
         </div>
+    </div>
+    <!-- Pagination UI -->
+    <div class="flex justify-center mt-4">
+      <button
+        @click="previous"
+        :disabled="page === 1"
+        class="px-4 py-2  rounded-l"
+      >
+        Prev
+      </button>
+      <button
+        @click="next"
+        :disabled="page === totalResults"
+        class="px-4 py-2  rounded-r"
+      >
+        Next
+      </button>
     </div>
 </template>
 
@@ -21,15 +42,34 @@
     export default {
         data() {
             return {
-                apiKey: '18951a20'
+                currentPage: this.page,
+                perPage: 10,
+                total: this.totalResults,
+                genre: 'action'
             }
         },
          computed: {
-            ...mapState(useMovieStore, ['movies']),
-            ...mapWritableState(useMovieStore, ['search'])
+            ...mapState(useMovieStore, ['movies', 'totalResults']),
+            ...mapWritableState(useMovieStore, ['search', 'releasedYear', 'page']),
         },
         methods: {
-            ...mapActions(useMovieStore, ['searchMovies'])
+            ...mapActions(useMovieStore, ['searchMovies', 'searchMoviesbyYear', 'searchMoviesByGenre', 'searchMoviesByIds', 'fill']),
+            previous() {
+                if(this.page > 1) {
+                    this.page--;
+                    this.fill();
+                } else {
+                    null
+                }
+            },
+            next() {
+                if (this.page < this.totalResults) {
+                    this.page++;
+                    this.fill();
+                } else {
+                    null
+                }
+            }
         },
     }
 </script>
--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -6,15 +6,38 @@ export const useMovieStore = defineStore("MovieStore", {
         return {
             search: '',
             movies: [],
-            apiKey: '18951a20'
+            apiKey: '18951a20',
+            releasedYear: '',
+            totalResults:'',
+            page: 1
         }
     },
     //actions
     actions: {
         fill() {
             const searchQuery = "Marvel";
+            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${searchQuery}&page=${this.page}`)
+            .then(res => res.json())
+            .then(data => {
+                this.totalResults = data.totalResults;
+                console.log(data)
+                if (data.Response === "True") {
+                    this.movies = data.Search;
+                    console.log(this.movies);
+                } else {
+                    // Handle error response
+                    console.error("Error fetching movies:", data.Error);
+                }
+            })
+            .catch(error => {
+                // Handle fetch error
+                console.error("Error fetching movies:", error);
+            });
+        },
 
-            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${searchQuery}`)
+        searchMovies() {
+            const q = this.search == 'number' ? 'y' : 's';
+            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&${q}=${this.search}`)
             .then(res => res.json())
             .then(data => {
                 if (data.Response === "True") {
@@ -31,8 +54,8 @@ export const useMovieStore = defineStore("MovieStore", {
             });
         },
 
-        searchMovies() {
-            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${this.search}`)
+        searchMoviesbyYear() {
+            fetch(`https://www.omdbapi.com/?apikey=${this.apiKey}&y=${this.releasedYear}`)
             .then(res => res.json())
             .then(data => {
                 if (data.Response === "True") {
@@ -47,7 +70,65 @@ export const useMovieStore = defineStore("MovieStore", {
                 // Handle fetch error
                 console.error("Error fetching movies:", error);
             });
-        }
+        },
+
+        searchMoviesByGenre() {
+            const apiKey = '18951a20';
+            const searchQuery = 'Marvel';
+            const genre = 'action'; // Replace with the desired genre
+        
+            fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`)
+              .then(res => res.json())
+              .then(data => {
+                if (data.Response === 'True') {
+                  // Filter movies by genre
+                  const filteredMovies = data.Search.filter(movie => movie.Genre && movie.Genre.includes(genre)); // Add null/undefined check
+                  this.movies = filteredMovies;
+                  console.log(this.movies);
+                } else {
+                  // Handle error response
+                  console.error('Error fetching movies:', data.Error);
+                }
+              })
+              .catch(error => {
+                // Handle fetch error
+                console.error('Error fetching movies:', error);
+              });
+          },
+            searchMoviesByIds(ids) { // Pass an array of IDs as argument
+            const apiKey = '18951a20';
+        
+            // Map array of IDs to an array of fetch promises
+            const fetchPromises = ids.map(id =>
+              fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+                .then(res => res.json())
+            );
+        
+            // Wait for all promises to resolve
+            Promise.all(fetchPromises)
+              .then(data => {
+                // Process movie details for each movie
+                const movies = data.map(movieData => {
+                  if (movieData.Response === 'True') {
+                    return movieData;
+                  } else {
+                    // Handle error response
+                    console.error(`Error fetching movie with ID ${movieData.imdbID}: ${movieData.Error}`);
+                    return null;
+                  }
+                });
+        
+                // Filter out null values (error responses)
+                const filteredMovies = movies.filter(movie => movie !== null);
+        
+                this.movies = filteredMovies;
+                console.log(this.movies);
+              })
+              .catch(error => {
+                // Handle fetch error
+                console.error('Error fetching movies:', error);
+              });
+          }
     }
 
     //getters
